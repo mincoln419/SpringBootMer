a6daf17e6a471d4a854d53d441fc5ebd582569d3
mincoln <mincoln419@naver>
2021-11-19 00:49:12 +0900

    message converter

---
+++ SpringBootMVC/src/main/java/com/mermer/user/User.java
@@ -0,0 +1,29 @@
+package com.mermer.user;
+
+public class User {
+
+	private Long id;
+	private String username;
+	private int age;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
--- SpringBootMVC/src/test/java/com/mermer/user/UserControllerTest.java
+++ SpringBootMVC/src/test/java/com/mermer/user/UserControllerTest.java
@@ -1,17 +1,21 @@
 package com.mermer.user;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.mermer.controller.UserContorller;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserContorller.class)
@@ -27,6 +31,18 @@ public class UserControllerTest {
 		.andExpect(status().isOk())
 		.andExpect(content().string("hello"));
 		
+	}
+	
+	@Test
+	public void createUser_JSON() throws Exception {
+		String userJson = "{\"username\":\"mermer\", \"age\": 27 }";
+		mockMvc.perform(post("/user/create")
+		.contentType(MediaType.APPLICATION_JSON_UTF8)
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.content(userJson))
+			.andExpect(status().isOk())
+			.andExpect((ResultMatcher) jsonPath("$.username", is(equalTo("mermer"))))
+			.andExpect(jsonPath("$.age", is(equalTo(27))));
 	}
 
 }
