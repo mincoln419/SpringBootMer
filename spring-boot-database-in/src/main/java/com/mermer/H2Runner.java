5d83521f1d2a2f1ef1dc460df42ed9de96ad6b47
mincoln <mincoln419@naver>
2021-11-24 02:46:06 +0900

    jpa...

---
+++ spring-boot-database-in/src/main/java/com/mermer/H2Runner.java
@@ -0,0 +1,50 @@
+package com.mermer;
+
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class H2Runner implements ApplicationRunner{
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	JdbcTemplate jdbc;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Connection con =dataSource.getConnection();
+		String url = con.getMetaData().getURL();
+		String username = con.getMetaData().getUserName();
+		
+		System.out.println(url + "," + username);
+		
+		Statement state =  con.createStatement();
+		
+		
+		String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+		try {
+			state.execute(sql);
+			System.out.println("create table");
+		}catch(Exception e) {
+			con.rollback();
+			System.out.println("rollback complete");
+		}
+		con.close();
+		
+		jdbc.execute("insert into user values(2, 'eunwoo')");
+	}
+	
+	
+	
+}
---
+++ spring-boot-database-in/src/main/java/com/mermer/SpringBootDatabaseInApplication.java
@@ -0,0 +1,13 @@
+package com.mermer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatabaseInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDatabaseInApplication.class, args);
+	}
+
+}
