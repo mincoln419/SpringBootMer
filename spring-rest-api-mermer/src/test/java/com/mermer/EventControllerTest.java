54d7da017aa881819f9245cb5f50eb99fb1e57b3
mincoln <mincoln419@naver.com>
2021-12-07 01:14:23 +0900

    restdocs base

--- spring-rest-api-mermer/src/test/java/com/mermer/EventControllerTest.java
+++ spring-rest-api-mermer/src/test/java/com/mermer/EventControllerTest.java
@@ -1,5 +1,6 @@
 package com.mermer;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -13,9 +14,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -23,6 +26,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mermer.common.RestDocConfiguration;
 import com.mermer.common.TestDescription;
 import com.mermer.events.Event;
 import com.mermer.events.EventDto;
@@ -40,6 +44,8 @@ import com.mermer.events.EventValidator;
 @SpringBootTest
 @AutoConfigureMockMvc
 //@MockMvcTest
+@AutoConfigureRestDocs
+@Import(RestDocConfiguration.class)
 public class EventControllerTest {
 
 	@Autowired
@@ -82,6 +88,7 @@ public class EventControllerTest {
 				.andExpect(jsonPath("_links.self").exists())
 				.andExpect(jsonPath("_links.query-events").exists())
 				.andExpect(jsonPath("_links.update-event").exists())
+				.andDo(document("create-event"));
 				;
 		
 	}
