04296d5afe153f4358d56a99e67ad7ed00982d67
mincoln <mincoln419@naver.com>
2021-12-14 22:48:43 +0900

    postgre + jpa applied

--- spring-boot-jpa/src/main/java/com/mermer/account/AccountRepository.java
+++ spring-boot-jpa/src/main/java/com/mermer/account/AccountRepository.java
@@ -1,8 +1,12 @@
 package com.mermer.account;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	Optional<Account> findByUsername(String username);
 
 	
 }
--- spring-boot-jpa/src/test/java/com/mermer/AccountRepositoryTest.java
+++ spring-boot-jpa/src/test/java/com/mermer/AccountRepositoryTest.java
@@ -1,19 +1,29 @@
 package com.mermer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.util.Optional;
+
 import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.function.SingletonSupplier;
 
+import com.mermer.account.Account;
 import com.mermer.account.AccountRepository;
 
 
 @RunWith(SpringRunner.class)
-@DataJpaTest//슬라이싱 테스트
+@DataJpaTest//Slicing 테스트
+//@SpringBootTest // Integration 테스트
 public class AccountRepositoryTest {
 	
 	
@@ -28,6 +38,25 @@ public class AccountRepositoryTest {
 	
 	@Test
 	public void di() {
+		try(Connection connection = dataSource.getConnection()){
+			DatabaseMetaData metaData = connection.getMetaData();
+			System.out.println(metaData.getURL());
+			System.out.println(metaData.getDriverName());
+			System.out.println(metaData.getUserName());
+		}catch(Exception e) {
+			
+		}
+		
+		Account account = Account.builder()
+				.username("mermer")
+				.password("pass")
+				.build();
+		
+		Account newAccount = accountRepository.save(account);
+		assertThat(newAccount).isNotNull();
+		
+		Optional<Account> exsitAccount = accountRepository.findByUsername(newAccount.getUsername());
+		assertThat(exsitAccount.orElseGet(null).getPassword()).isEqualTo(account.getPassword());
 		
 	}
 	
