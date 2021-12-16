4545adafeb22abd54eaa5cd7a84440e0fc22358d
mincoln <mincoln419@naver.com>
2021-12-16 13:59:27 +0900

    spring-boot secruity

---
+++ spring-rest-api-mermer-security/src/main/java/com/mermer/controller/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.mermer.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+}
