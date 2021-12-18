6658773f0ce32c0c0ede431bf4c0401a97e36707
mincoln <mincoln419@naver.com>
2021-12-19 01:45:30 +0900

    git cached rm

--- spring-mermer-shop-mvc/src/test/java/com/mermer/cm/config/AuthServerConfigTest.java
+++ spring-mermer-shop-mvc/src/test/java/com/mermer/cm/config/AuthServerConfigTest.java
@@ -1,19 +1,26 @@
+
+package com.mermer.cm.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /**
  * @packageName : com.mermer.cm.config
  * @fileName : AuthServerConfigTest.java 
  * @author : Mermer 
  * @date : 2021.12.19 
- * @description :
+ * @description : 사용자 인증 테스트
  * =========================================================== 
  * DATE AUTHOR NOTE 
  * ----------------------------------------------------------- 
  * 2021.12.19 Mermer 최초 생성
  */
-package com.mermer.cm.config;
-
-/* 
- * @description: 
- */
 public class AuthServerConfigTest {
-
+	
+	@Test
+	@DisplayName("인증토큰 발급 테스트")
+	public void getAuthToken() {
+		
+	}
+	
 }
