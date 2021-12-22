1da45c0f1b08ed1d59d4e96e222dabb22ffe54b6
mermer <mincoln419@naver.com>
2021-12-22 21:26:48 +0900

    #19 - view 연결 기본 설정
    #19 view 연결기본 설정
    - auth
    - notice
    - bulletin

---
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/view/controller/AuthController.java
@@ -0,0 +1,30 @@
+
+package com.mermer.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @packageName : com.mermer.view.controller
+ * @fileName : AuthController.java 
+ * @author : Mermer 
+ * @date : 2021.12.22 
+ * @description : 회원가입/로그인 핸들러 매소드 매핑
+ * =========================================================== 
+ * DATE AUTHOR NOTE 
+ * ----------------------------------------------------------- 
+ * 2021.12.22 Mermer 최초 생성
+ */
+@Controller
+public class AuthController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "auth/login"; // 설정된 경로의 login.jsp 또는 login.html 화면으로 리턴 
+	}
+	
+	@GetMapping("/sign-up")
+	public String signUp() {
+		return "auth/signUp"; // 설정된 경로의 login.jsp 또는 login.html 화면으로 리턴 
+	}
+}
---
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/view/controller/NoticeController.java
@@ -0,0 +1,48 @@
+
+package com.mermer.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ * @packageName : com.mermer.view.controller
+ * @fileName : NoticeController.java 
+ * @author : Mermer 
+ * @date : 2021.12.22 
+ * @description :
+ * =========================================================== 
+ * DATE AUTHOR NOTE 
+ * ----------------------------------------------------------- 
+ * 2021.12.22 Mermer 최초 생성
+ */
+@Controller("/notice")
+public class NoticeController {
+	
+	@GetMapping
+	public String noticeList() {
+	
+		return "notice/list";
+	}
+	
+	@GetMapping("/{id}")
+	public String noticeDetail(@PathVariable Long id) {
+		
+		return "/notice/detail";
+	}
+	
+	@PostMapping
+	public String noticeCreate() {
+		
+		return "/notice/edit";
+	}
+	
+	@PutMapping("/{id}")
+	public String noticeDetailEdit(@PathVariable Long id) {
+		
+		return "/notice/edit";
+	}
+
+}
