f23d1a0071f77245a3bbdfb4244a57a038427170
mermer <mincoln419@naver.com>
2021-12-22 21:31:19 +0900

    #19 - calculator view 연결
    #19 - calculator view 연결

---
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/view/controller/CalculatorController.java
@@ -0,0 +1,25 @@
+/**
+ * @packageName : com.mermer.view.controller
+ * @fileName : CalculatorController.java 
+ * @author : Mermer 
+ * @date : 2021.12.22 
+ * @description :
+ * =========================================================== 
+ * DATE AUTHOR NOTE 
+ * ----------------------------------------------------------- 
+ * 2021.12.22 Mermer 최초 생성
+ */
+package com.mermer.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/calulator")
+public class CalculatorController {
+
+	@GetMapping
+	public String lawCal() {
+		return "main";
+	}
+	
+}
