fc3106ce0488c34fc2bd60e16970fa5c3f675eaf
mermer <mincoln419@naver.com>
2021-12-23 22:43:08 +0900

    주석 수정
    주석 수정

--- spring-mermer-shop-mvc/src/main/java/com/mermer/cm/resource/AccountResource.java
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/cm/resource/AccountResource.java
@@ -6,7 +6,17 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 
 import com.mermer.cm.entity.Account;
-
+/**
+ * @packageName : com.mermer.cm.resource
+ * @fileName : NoticeResource.java 
+ * @author : Mermer 
+ * @date : 2021.12.18 
+ * @description : return body 역할 해줄 resource
+ * =========================================================== 
+ * DATE AUTHOR NOTE 
+ * ----------------------------------------------------------- 
+ * 2021.12.18 Mermer 최초 생성
+ */
 public class AccountResource extends EntityModel<Optional<Account>>{
 	
 }
