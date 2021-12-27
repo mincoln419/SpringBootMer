8c8f460407d07c4d016158514fe42a23534448cd
Mincoln Cho <32834468+mincoln419@users.noreply.github.com>
2021-12-28 01:41:50 +0900

    Delete spring-boot-jpa directory

--- spring-boot-jpa/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists account cascade;
-drop sequence if exists hibernate_sequence;
-create sequence hibernate_sequence start 1 increment 1;
-create table account (id int8 not null, password varchar(255), username varchar(255), primary key (id));
--- spring-boot-jpa/src/main/resources/db/migration/V2__add.active.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE account ADD COLUMN active BOOLEAN;
-
--- 데이터 변경도 가능하고... 기존의 DDL 이력을 남길 수 있다는 점에서 매우 유용함
