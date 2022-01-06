c1293ab53468168431301495296c36c786606c75
mermer <mincoln419@naver.com>
2022-01-07 01:52:10 +0900

    #42 이미지 업로드 프론트엔드 작업
    #42 이미지 업로드 프론트엔드 작업 완료..
    리사이징 해서 미리보기 기능 추가 예정..

--- spring-mermer-shop-mvc/frontend/src/components/NoticeForm.js
+++ spring-mermer-shop-mvc/frontend/src/components/NoticeForm.js
@@ -3,9 +3,12 @@ import React, { useCallback, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addPost } from '../reducer/notice';
 import { DownOutlined , UserOutlined} from '@ant-design/icons';
+import {UPLOAD_IMAGE_REQUEST} from '../actions/notice'
 
 const NoticeForm = () => {
     const {images} = useSelector((state) => state.notice);
+    const {token} = useSelector((state) => state.user.Account);
+    console.log("token", token);
     const [text, setText] = useState('');
     const dispatch = useDispatch();
     const imageInput = useRef();
@@ -18,9 +21,26 @@ const NoticeForm = () => {
         setText('');
     }, []);
 
+    //업로드 버튼 눌렀을 때 imageInput에 값 넣기 -> array로 
     const onClickImageUpload = useCallback(()=>{
-        imageInput.current.click((e) => console.log(e));
+        imageInput.current.click();
     }, [imageInput.current]);
+
+    //업로드 화면에서 파일 선택했을 때 formData에 담아 upload -> 미리보기로 리사아즈
+    const onChangeImages = useCallback((e) => {
+        const imageFormData = new FormData();
+        console.log(e.target.files);
+        [].forEach.call(e.target.files, (f) => {
+            imageFormData.append('images', f);
+        });
+        imageFormData.append('name', 'notice');
+
+        dispatch({
+            type: UPLOAD_IMAGE_REQUEST,
+            tmpImgs: imageFormData,
+            token : token,
+        });
+    });
 
     return (
 
@@ -33,7 +53,7 @@ const NoticeForm = () => {
                 placeholder='내용을 입력하세요'
             />
             <div>
-                <input type="file" multiple hidden ref={imageInput}/>
+                <input type="file" multiple hidden ref={imageInput} onChange ={onChangeImages}/>
                 <Button onClick={onClickImageUpload}>이미지 업로드</Button>
                 <Button type="primary" style={{float:'right'}} htmlType="submit">작성완료</Button>
 
