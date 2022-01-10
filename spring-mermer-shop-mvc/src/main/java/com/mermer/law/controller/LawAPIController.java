46e4bdd131f2686435142c103dad6d8748af7fda
mermer <mincoln419@naver.com>
2022-01-11 01:37:07 +0900

    #54-law-buisness-적용
    #54-law-buisness-entity 생성 및 컨트롤러 생성
    -law domain

---
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/law/controller/LawAPIController.java
@@ -0,0 +1,43 @@
+
+package com.mermer.law.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mermer.cm.entity.Account;
+import com.mermer.cm.entity.dto.NoticeDto;
+import com.mermer.cm.util.CurrentUser;
+
+/**
+ * @packageName : com.mermer.law.entity
+ * @fileName : LawAPIController.java 
+ * @author : Mermer 
+ * @date : 2022.01.11 
+ * @description : 공개법률정보 시스템에서 API로 데이터를 받아오는 Controller - 데이터를 호출하여 API서버에 저장시키기 위한 Controller -> create만 가능
+ * =========================================================== 
+ * DATE AUTHOR NOTE 
+ * ----------------------------------------------------------- 
+ * 2022.01.11 Mermer 최초 생성
+ */
+@RestController
+public class LawAPIController {
+
+	
+	@PostMapping
+	public ResponseEntity callLawInfo(HttpServletRequest req,
+			   @RequestBody @Validated LawDto lawDto,
+			   Errors errors,
+			   @CurrentUser Account account
+			) {
+		
+		return null;
+	}
+	
+	
+}
---
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/law/entity/LawDomain.java
@@ -0,0 +1,65 @@
+
+package com.mermer.law.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.mermer.cm.entity.Account;
+import com.mermer.cm.entity.embeded.CommonEmbeded;
+import com.mermer.cm.entity.type.AccountPart;
+import com.mermer.cm.entity.type.AccountRole;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @packageName : com.mermer.law.entity
+ * @fileName : LawDomain.java 
+ * @author : Mermer 
+ * @date : 2022.01.11 
+ * @description : 법률 도메인 엔티티
+ * =========================================================== 
+ * DATE AUTHOR NOTE 
+ * ----------------------------------------------------------- 
+ * 2022.01.11 Mermer 최초 생성
+ */
+
+
+@Entity @NoArgsConstructor @AllArgsConstructor
+@Table(name = "LW_TB_DOMAIN")
+@SuperBuilder @Getter @Setter @EqualsAndHashCode(of = "id", callSuper = false)
+@EntityListeners(AuditingEntityListener.class)
+@SequenceGenerator(name = "LW_SQ_ID_GENERATOR", sequenceName = "LW_SQ_ID_GENERATOR", initialValue = 1, allocationSize = 1)
+public class LawDomain extends CommonEmbeded{
+
+	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LW_SQ_ID_GENERATOR")
+	@Column(name = "DOMAIN_ID")
+	private Long id;
+	
+	@NotBlank
+	private String lawDomainName;//DB에 저장시킬 법률명
+	@NotBlank
+	private String lawDomainCode;//공개법률정보에서 사용하는 법률코드
+	
+	@Column(name = "LAW_CONTENT")
+	@Lob
+	private String content;
+	
+	
+}
