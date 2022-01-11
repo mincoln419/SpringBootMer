02f12bc8d00fbad96554b1855e4c8671b28450b4
mermer <mincoln419@naver.com>
2022-01-11 18:27:03 +0900

    #54 law buisness entity 작업
    #54 law buisness entity 작업

--- spring-mermer-shop-mvc/src/main/java/com/mermer/law/entity/LawInstance.java
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/law/entity/LawInstance.java
@@ -1,25 +1,17 @@
 
 package com.mermer.law.entity;
 
-import java.util.Set;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import com.mermer.cm.entity.Account;
 import com.mermer.cm.entity.embeded.CommonEmbeded;
-import com.mermer.cm.entity.type.AccountPart;
-import com.mermer.cm.entity.type.AccountRole;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -30,36 +22,25 @@ import lombok.experimental.SuperBuilder;
 
 /**
  * @packageName : com.mermer.law.entity
- * @fileName : LawDomain.java 
+ * @fileName : LawInstance.java 
  * @author : Mermer 
  * @date : 2022.01.11 
- * @description : 법률 도메인 엔티티
+ * @description :
  * =========================================================== 
  * DATE AUTHOR NOTE 
  * ----------------------------------------------------------- 
  * 2022.01.11 Mermer 최초 생성
  */
-
-
 @Entity @NoArgsConstructor @AllArgsConstructor
-@Table(name = "LW_TB_DOMAIN")
+@Table(name = "LW_TB_INSTANCE")
 @SuperBuilder @Getter @Setter @EqualsAndHashCode(of = "id", callSuper = false)
 @EntityListeners(AuditingEntityListener.class)
-@SequenceGenerator(name = "LW_SQ_DOMAIN_ID_GENERATOR", sequenceName = "LW_SQ_DOMAIN_ID", initialValue = 1, allocationSize = 1)
-public class LawDomain extends CommonEmbeded{
-
-	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LW_SQ_DOMAIN_ID_GENERATOR")
-	@Column(name = "DOMAIN_ID")
+@SequenceGenerator(name = "LW_SQ_INSTANCE_ID_GENERATOR", sequenceName = "LW_SQ_ID_GENERATOR", initialValue = 1, allocationSize = 1)
+public class LawInstance extends CommonEmbeded{
+	
+	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LW_SQ_INSTANCE_ID_GENERATOR")
 	private Long id;
 	
-	@NotBlank
-	private String lawDomainName;//DB에 저장시킬 법률명
-	@NotBlank
-	private String lawDomainCode;//공개법률정보에서 사용하는 법률코드
-	
-	@Column(name = "LAW_CONTENT")
-	@Lob
-	private String content;
 	
 	
 }
--- spring-mermer-shop-mvc/src/main/java/com/mermer/law/entity/dto/LawDto.java
+++ spring-mermer-shop-mvc/src/main/java/com/mermer/law/entity/dto/LawDto.java
@@ -6,7 +6,7 @@ package com.mermer.law.entity.dto;
  * @fileName : LawDto.java 
  * @author : Mermer 
  * @date : 2022.01.11 
- * @description : 법률 정보 요청시에 사용하는 기본 항목에 dto
+ * @description : 법률 정보 요청시에 사용하는 기본 항목에 dto - validation하기 위함 -> 공개법률정보 시스템 api 호출하기전에 validation하기 위함
  * =========================================================== 
  * DATE AUTHOR NOTE 
  * ----------------------------------------------------------- 
