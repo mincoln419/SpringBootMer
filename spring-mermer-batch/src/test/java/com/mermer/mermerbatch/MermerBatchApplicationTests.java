41a14b4af9ee6571fe0a97f4c1048fca205e90d4
mermer <mincoln419@naver.com>
2022-01-14 15:21:39 +0900

    #67- gradle-spring-boot 세팅
    #67- gradle-spring-boot 세팅

---
+++ spring-mermer-batch/src/test/java/com/mermer/mermerbatch/MermerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mermer.mermerbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MermerBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
